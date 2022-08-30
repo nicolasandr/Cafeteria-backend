e758ccdd776e49fb2a355ac20d5d8d2bd1ec579e
nicolas andrade <nicolasandrade328@gmail.com>
2022-08-30 18:04:37 -0300

    agregamos la configuracion de nodemon

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 import express from 'express';
 //const express = require('express');
+
+//instancia de express
+const app = express();
+
+//queremos tomar un puerto de la pc
+app.set('port',process.env.PORT || 4000);
+
+//quiero que mi backend escuche el puerto 
+app.listen(app.get('port'), ()=>{
+        console.log('estamos en el puerto '+app.get('port') )
+})
 console.log('hola mundo')
