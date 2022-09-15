820ad15f356cf0c766b67b6bf7de7d8ee71a5986
nicolas andrade <nicolasandrade328@gmail.com>
2022-09-15 17:31:52 -0300

    agregamos las validaciones con express validator

--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { check } from "express-validator";
 import { borrarProducto, crearProducto, listarProductos, editarProducto, obtenerProducto } from "../controllers/productos.controller";
 const router  = Router();
 
@@ -7,7 +8,17 @@ const router  = Router();
 router
 .route('/productos')
 .get(listarProductos)
-.post(crearProducto)
+.post([check('nombreProducto','El nombre del producto es obligatorio').notEmpty(),
+check('nombreProducto','El Producto debe tener entre 2 y 10 caracteres').isLength({min:2,max:50}),
+check('precio','El precio es un valor obligatorio').notEmpty(),
+check('precio').custom((value)=>{
+        if(value>=0 && value <=9000){
+                return true;
+        }
+        else{
+                throw new Error('El precio debe estar entre 0 y 9000')
+        }
+})],crearProducto)
 
 router.route('/productos/:id').get(obtenerProducto).put(editarProducto).delete(borrarProducto);
 
